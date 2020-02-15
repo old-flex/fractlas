0cba2da5c4f946ef0caa6a57e2ebfe0a19e83a22
old-flex <kostyaniusgaming@gmail.com>
2020-02-15 19:33:30 +0700

    Добавил переменные

--- graphic.py
+++ graphic.py
@@ -1,1 +1,19 @@
-print("Миша пидорас")
+# import some modules
+import numpy as np
+import matplotlib.pyplot as plt
+
+realMin = -2.5
+realMax = 1.5
+
+imaginaryMin = -2
+imaginaryMax = 2
+
+horizontalPointsNumber = 200
+verticalPointsNumber = 200
+
+max_iterations = 300
+
+maxInfinityNumber = 10
+
+image = np.zeros((horizontalPointsNumber, verticalPointsNumber))
+
