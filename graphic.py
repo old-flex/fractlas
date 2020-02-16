35ae93fed6a9c03769001ed14c4ac7ad35ce8154
old-flex <kostyaniusgaming@gmail.com>
2020-02-17 01:22:01 +0700

    Init commit

--- graphic.py
+++ graphic.py
@@ -8,12 +8,21 @@ realMax = 1.5
 imaginaryMin = -2
 imaginaryMax = 2
 
-horizontalPointsNumber = 200
-verticalPointsNumber = 200
+realPoints = 200
+imaginaryPoints = 200
 
 max_iterations = 300
 
 maxInfinityNumber = 10
 
-image = np.zeros((horizontalPointsNumber, verticalPointsNumber))
+image = np.zeros((realPoints, imaginaryPoints))
+
+realPart, imaginaryPart = np.mgrid[realMin:realMax:(realPoints * 1j), imaginaryMin:imaginaryMax:(imaginaryPoints * 1j)]
+
+complexNumber = realPart + 1j * imaginaryPart
+newComplex = np.zeros_like(complexNumber)
+
+for i in range(max_iterations):
+    newComplex = newComplex ** 2 + complexNumber
+
 
